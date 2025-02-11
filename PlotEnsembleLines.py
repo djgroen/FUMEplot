a7ec51baad709d62a650fafe51f70f2022aae614
JonMcCullough <j.mccullough@ucl.ac.uk>
2025-02-11 10:35:14 +0000

    Add standard deviation plotting function

--- PlotEnsembleLines.py
+++ PlotEnsembleLines.py
@@ -49,6 +49,28 @@ def plotFleeCamp(plot_num, sim_index, data_index):
     plt.ylabel('# of asylum seekers or unrecognized refugees')
     plt.title(str(headers[data_index]))
 
+def plotFleeCampSTDBound(plot_num, sim_index, data_index):    
+    ensembleSize = 0
+    dfTest = []
+    # loop through each ensemble job extracting sim data and assigning to df for each campsite
+    for name in os.listdir(outdir):
+        df = pd.read_csv(f"{outdir}/{name}/out.csv")
+        dfTest.append(df.iloc[:, sim_index].T)
+        ensembleSize += 1
+    
+    # plot all waveforms
+    plt.figure(plot_num+1)
+    
+    plt.plot(np.mean(dfTest,axis=0),'maroon',label='ensemble mean')
+    plt.fill_between(np.linspace(0,len(dfTest[0]),len(dfTest[0])), 
+                                 np.mean(dfTest,axis=0) - np.std(dfTest,axis=0), 
+                                 np.mean(dfTest,axis=0) + np.std(dfTest,axis=0), 
+                                 where=np.ones(len(dfTest[0])), alpha=0.3, color='maroon', label=r'mean $\pm$ 1 std')
+    plt.plot(df.iloc[:, data_index],'b-', label='UN Data')
+    plt.legend()
+    plt.xlabel('Day')
+    plt.ylabel('# of asylum seekers or unrecognized refugees')
+    plt.title(str(headers[data_index]))
 
 #main plotting script
 if __name__ == "__main__":
@@ -60,6 +82,7 @@ if __name__ == "__main__":
 
     for i in range(len(sim_indices)):
         plotFleeCamp(i, sim_indices[i], data_indices[i])
+        plotFleeCampSTDBound(10*i, sim_indices[i], data_indices[i])
 
     #plot mean against quartile range for uncertainty
     plt.show()
