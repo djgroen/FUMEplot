b1a615059202a72b4a6c3aebb143d71491770112
Yehor Yudin <yehor.yudin@proton.me>
2025-03-13 17:51:48 +0100

    Single Histogram plotting: added more info and converted to boxplot

--- fumeplot/PlotNamedSingleByTimestep.py
+++ fumeplot/PlotNamedSingleByTimestep.py
@@ -10,6 +10,14 @@ import matplotlib.patches as mpatches
 from matplotlib.backends.backend_pdf import PdfPages
 from contextlib import nullcontext
 
+
+def _formatLabels(labels):
+
+    # Capitalize and replace underscores with spaces
+    labels =  [label.replace('_', ' ').title() for label in labels]
+
+    return labels
+
 def plotCounts(plot_num, all_counts, save_fig, plot_folder, combine_plots_pdf):
     # Combine counts into a DataFrame, filling missing values with 0
     combined_counts = pd.DataFrame(all_counts).fillna(0)
@@ -19,22 +27,55 @@ def plotCounts(plot_num, all_counts, save_fig, plot_folder, combine_plots_pdf):
     std_counts = combined_counts.std()
 
     # Plot histogram with error bars
-    fig = plt.figure(plot_num+1, figsize=(10, 6))
-    plt.bar(mean_counts.index, mean_counts.values, yerr=std_counts.values, color='skyblue', capsize=5)
+    fig = plt.figure(plot_num+1, figsize=(15, 9))
+
+    # plt.bar(mean_counts.index, 
+    #         mean_counts.values, 
+    #         yerr=std_counts.values, 
+    #         color='skyblue', 
+    #         capsize=5,
+    #         )
+
+    plt.boxplot(
+                combined_counts,
+                tick_labels=_formatLabels(mean_counts.index),
+                patch_artist=True, 
+                #notch=True,
+                bootstrap=1000,
+                showmeans=True, 
+                meanline=True, 
+                showbox=True,
+                showcaps=True,
+                showfliers=True,
+                medianprops=dict(color='blue'),
+                meanprops=dict(color='black'),
+                boxprops=dict(facecolor='skyblue'),
+                whiskerprops=dict(color='blue'),
+                capprops=dict(color='blue',),
+                capwidths=0.3,
+                flierprops=dict(marker='o', markerfacecolor='blue', markersize=2, linestyle='none'),
+                label='Entries',
+                )
 
     # Labels and title
-    plt.xlabel('Source Country')
+    plt.legend(
+        [mpatches.Patch(color='black', label='Mean'), 
+         mpatches.Patch(color='blue', label='Median'), 
+         mpatches.Patch(color='skyblue', label='Q1-Q3'),], 
+        ['Mean', 'Median', '25th-75th percentile'])
+    plt.xlabel('Source Location')
     plt.ylabel('Number of Entries')
-    plt.title('Histogram of Entries Grouped by Source')
+    plt.title('Boxplot of Entries Grouped by Source')
     plt.xticks(rotation=45)
+    #plt.axis('tight')
+    plt.tight_layout()
 
     if combine_plots_pdf:
         combine_plots_pdf.savefig(fig)
         print(f"Saved plot {plot_num} to PDF.")
     if save_fig:
         plt.savefig(plot_folder+'/EntriesBySource.png')
-
-
+    #plt.close()
 
 def _getFilteredCounts(csv_files, query, var_type="str"):
 
@@ -66,7 +107,6 @@ def _getFilteredCounts(csv_files, query, var_type="str"):
 
     return all_counts
 
-
 def plotSourceHist(outdirs, filters, save_fig=False, plot_folder=None, combine_plots_pdf=False):
 
     # Read and aggregate data from multiple CSV files
@@ -93,10 +133,10 @@ def plotSourceHist(outdirs, filters, save_fig=False, plot_folder=None, combine_p
 
     if not combine_plots_pdf:
         plt.show()
+    #plt.close()
     
 def plotNamedSingleByTimestep(code, outdir, plot_type, FUMEheader, filters=[]):
     plotSourceHist(outdir, filters, save_fig=False, plot_folder=None, combine_plots_pdf=FUMEheader.combine_plots_pdf)
-
 
 
 if __name__ == "__main__":
@@ -133,3 +173,5 @@ if __name__ == "__main__":
 
 # ISSUES:
 # - Flee and Facs require _agentlog folders to be present
+
+# error bar: add max-min interval, 25-75 percentiles, std(?)
