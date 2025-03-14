db405b50f5b332dea66f197157e08b34ccddbf77
Yehor Yudin <yehor.yudin@proton.me>
2025-03-14 15:12:25 +0100

    PlotNamedStocksByTimestep.py: formatting the plot, adding labels, grids etc.

--- fumeplot/PlotNamedStocksByTimestep.py
+++ fumeplot/PlotNamedStocksByTimestep.py
@@ -6,6 +6,7 @@ import os
 import sys
 from pathlib import Path
 import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 
 from matplotlib.backends.backend_pdf import PdfPages
 from contextlib import nullcontext
@@ -21,6 +22,7 @@ def _formatLabels(labels):
 def plotLocation(outdirs, plot_num, loc_index, sim_index, data_index, loc_names, y_label, save_fig=False, plot_folder=None, combine_plots_pdf=False):    
     ensembleSize = 0
     dfTest = []
+
     # loop through each ensemble job extracting sim data and assigning to df for each location
     for d in outdirs:
         df = pd.read_csv(f"{d}/out.csv")
@@ -30,19 +32,34 @@ def plotLocation(outdirs, plot_num, loc_index, sim_index, data_index, loc_names,
     # plot all waveforms
     fig = plt.figure(plot_num+1)
     
+    # plot individual ensemble members
     for i in range(ensembleSize):
-        plt.plot(dfTest[i],'k', alpha=0.2)
+        plt.plot(dfTest[i],'k', alpha=0.15)
         #print(f"size of dfTest[i]: {len(dfTest[i])}") #debugging
-    plt.plot(np.mean(dfTest,axis=0),'maroon',label='Ensemble Mean')
+
+    # plot ensemble mean
+    plt.plot(np.mean(dfTest,axis=0), 'maroon', label='Ensemble Mean')
+
+    # plot the reference data if available
     if data_index > 0:
-        plt.plot(df.iloc[:, data_index],'b-', label='UN Data')
+        plt.plot(df.iloc[:, data_index], 'b-', label='UN Data')
         #print(f"sizes of dfTest and data: {len(dfTest[0])}, {len(df.iloc[:, data_index])}") #debugging
-    plt.legend()
+
+    # set up legend
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handles.append(mlines.Line2D([], [], color='black', linestyle='-', label='Ensemble Members'))
+    labels.append('Ensemble Members')
+    plt.legend(handles=handles, labels=labels, loc='best')
+    
+    # set up formatting
     plt.xlabel('Day')
     plt.ylabel(y_label)
     plt.title(str(loc_names[loc_index]))
+
+    plt.grid(visible=True, which='both', axis='both', linestyle='-', linewidth=0.33,)
     plt.tight_layout()
     
+    # save the plot
     if combine_plots_pdf:
         combine_plots_pdf.savefig(fig)
     if save_fig:
@@ -53,6 +70,7 @@ def plotLocation(outdirs, plot_num, loc_index, sim_index, data_index, loc_names,
 def plotLocationSTDBound(outdirs, plot_num, loc_index, sim_index, data_index, loc_names, y_label, save_fig=False, plot_folder=None, combine_plots_pdf=False):
     ensembleSize = 0
     dfTest = []
+
     # loop through each ensemble job extracting sim data and assigning to df for each location
     for d in outdirs:
         df = pd.read_csv(f"{d}/out.csv")
@@ -62,19 +80,34 @@ def plotLocationSTDBound(outdirs, plot_num, loc_index, sim_index, data_index, lo
     # plot all waveforms
     fig = plt.figure(plot_num+1)
     
+    # plot simulation ensemble mean
     plt.plot(np.mean(dfTest,axis=0),'maroon',label='Ensemble Mean')
+
+    # plot simulation ensemble mean +/- 1 standard deviation
     plt.fill_between(np.linspace(0,len(dfTest[0]),len(dfTest[0])), 
                                  np.mean(dfTest,axis=0) - np.std(dfTest,axis=0), 
                                  np.mean(dfTest,axis=0) + np.std(dfTest,axis=0), 
                                  where=np.ones(len(dfTest[0])), alpha=0.3, color='maroon', label=r'Mean $\pm$ 1 STD')
+    #TODO: add 2,3 std deviations, percentiles, absolute min/max, mean error etc.
+    plt.fill_between(np.linspace(0,len(dfTest[0]),len(dfTest[0])),
+                                np.max(dfTest,axis=0),
+                                np.min(dfTest,axis=0),
+                                where=np.ones(len(dfTest[0])), alpha=0.1, color='maroon', label='Min - Max')
+    
+    # plot reference data if available
     if data_index > 0:
         plt.plot(df.iloc[:, data_index],'b-', label='UN Data')
-    plt.legend()
+
+    # set up formatting
+    plt.legend(loc='best')
     plt.xlabel('Day')
     plt.ylabel(y_label)
     plt.title(str(loc_names[loc_index]))
+
+    plt.grid(visible=True, which='both', axis='both', linestyle='-', linewidth=0.33,)
     plt.tight_layout()
      
+    # save the plot
     if combine_plots_pdf:
         combine_plots_pdf.savefig(fig)
     if save_fig:
@@ -85,6 +118,7 @@ def plotLocationSTDBound(outdirs, plot_num, loc_index, sim_index, data_index, lo
 def plotLocationDifferences(outdirs, plot_num, loc_index, sim_index, data_index, loc_names, save_fig=False, plot_folder=None, combine_plots_pdf=False):    
     ensembleSize = 0
     dfTest = []
+
     # loop through each ensemble job extracting sim data and assigning to df for each location
     for d in outdirs:
         df = pd.read_csv(f"{d}/out.csv")
@@ -93,6 +127,7 @@ def plotLocationDifferences(outdirs, plot_num, loc_index, sim_index, data_index,
     
     rmse = np.sqrt(((np.mean(dfTest,axis=0) - df.iloc[:, data_index]) ** 2).mean())
     ard = np.abs(np.mean(dfTest,axis=0) - df.iloc[:, data_index]).mean()
+    
     # add text box for the statistics
     stats = (f'RMSE = {rmse:.2f}\n'
              f'ARD = {ard:.2f}')
@@ -110,6 +145,7 @@ def plotLocationDifferences(outdirs, plot_num, loc_index, sim_index, data_index,
     plt.title(str(loc_names[loc_index]))
     plt.tight_layout()
  
+    # save the plot
     if combine_plots_pdf:
         combine_plots_pdf.savefig(fig)
     if save_fig:
@@ -123,18 +159,11 @@ def adjacent_values(vals, q1, q3):
     lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
     return lower_adjacent_value, upper_adjacent_value
 
-def adjacent_values(vals, q1, q3):
-    upper_adjacent_value = q3 + (q3 - q1) * 1.5
-    upper_adjacent_value = np.clip(upper_adjacent_value, q3, vals[-1])
-
-    lower_adjacent_value = q1 - (q3 - q1) * 1.5
-    lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
-    return lower_adjacent_value, upper_adjacent_value
-
 def animateLocationHistogram(outdirs, plot_num, loc_index, sim_index, data_index, loc_names, x_label, save_fig=False, plot_folder=None, combine_plots_pdf=False):    
     ensembleSize = 0
     maxPop = 0
     dfTest = []
+
     # loop through each ensemble job extracting sim data and assigning to df for each location
     for d in outdirs:
         df = pd.read_csv(f"{d}/out.csv")
@@ -155,7 +184,8 @@ def animateLocationHistogram(outdirs, plot_num, loc_index, sim_index, data_index
         ax.set_title(str(loc_names[loc_index] + ' - Day '+ str(i)))
         ax.set(xlim=[0, 1.1*maxPop], ylim=[0, ensembleSize], xlabel=x_label, ylabel='Ensemble Observations')
         #ax.set(ylim=[0, 10], xlabel='# Refugees', ylabel='Occurances')
-        ax.legend()
+        ax.grid(visible=True, which='both', axis='both', linestyle='-', linewidth=0.33,)
+        ax.legend() 
         if data_index > 0:
             return (hist, data)
         else:
@@ -166,16 +196,20 @@ def animateLocationHistogram(outdirs, plot_num, loc_index, sim_index, data_index
 
     ax.hist([item[0] for item in dfTest], bins=10, color='c', edgecolor='k', alpha=0.65, label='Ensemble Data')
 
+    # plot the reference data if available
     if data_index > 0:
         ax.axvline(df.iloc[0, data_index], color='k', linestyle='dashed', linewidth=1, label='UN Data')
     ax.set_title(str(loc_names[loc_index] + ' - Day '+ str(0)))
 
-    # set 
+    # set up formatting
     ax.set(xlim=[0, 1.1*maxPop], ylim=[0, ensembleSize], xlabel=x_label, ylabel='Ensemble Observations')
+    #plt.xticks(rotation=45)
+    ax.grid(visible=True, which='both', axis='both', linestyle='-', linewidth=0.33,)
     ax.legend()
     
     ani = animation.FuncAnimation(fig, updatehist, len(dfTest[0]))
-        
+
+    # save the plot   
     if save_fig:
        ani.save(filename=plot_folder+'/'+str(loc_names[loc_index]).replace(" ", "").replace("#", "Num")+'_Histogram.gif', writer="pillow")
 
@@ -185,6 +219,7 @@ def animateLocationViolins(outdirs, plot_num, i, sim_indices, data_indices, loc_
     ensembleSize = 0
     maxPop = 0
     dfFull = []
+
     # loop through each ensemble job extracting sim data and assigning to df for each location
     for d in outdirs:
         dfTest=[]
@@ -225,6 +260,7 @@ def animateLocationViolins(outdirs, plot_num, i, sim_indices, data_indices, loc_
             showextrema=False,
             )
 
+        # plot the reference data if available
         if dataAvailable:
             ax.scatter([y + 1 for y in range(len(sim_indices))], dataValues.iloc[i].values, color='r', label='UN Data')
             ax.vlines([y + 1 for y in range(len(sim_indices))], quartile1, quartile3, color='k', linestyle='-', lw=1)
@@ -235,12 +271,13 @@ def animateLocationViolins(outdirs, plot_num, i, sim_indices, data_indices, loc_
             handles.append(mpatches.Patch(color='C0', label='Simulations')) 
             ax.legend(handles=handles)
 
+        # set up formatting
         ax.set_title(f"{y_label} - Day {i}")
-        ax.set(xlabel='Category', ylabel='Observations')
+        ax.set(xlabel='Category', ylabel='# of Observations')
         ax.yaxis.grid(True)
         ax.set_xticks([y+1 for y in range(len(sim_indices))])
         ax.set_xticklabels(loc_names) #as old version of matplotlib
-        ax.tick_params(axis='x', rotation=90)
+        ax.tick_params(axis='x', rotation=45)
         plt.tight_layout()
 
         return (hist)
@@ -260,6 +297,7 @@ def animateLocationViolins(outdirs, plot_num, i, sim_indices, data_indices, loc_
 
     ax.violinplot(locData, showmeans=False, showmedians=True)
 
+    # plot the reference data if available
     if dataAvailable:
         ax.scatter([y + 1 for y in range(len(sim_indices))], dataValues.iloc[0].values, color='r', label='UN Data')
         ax.vlines([y + 1 for y in range(len(sim_indices))], quartile1, quartile3, color='k', linestyle='-', lw=1)
@@ -270,15 +308,17 @@ def animateLocationViolins(outdirs, plot_num, i, sim_indices, data_indices, loc_
         handles.append(mpatches.Patch(color='C1', label='Simulations')) 
         ax.legend(handles=handles)
         
+    # set up formatting
     ax.set_title(f"{y_label} - Day {i}")
-    ax.set(xlabel='Category', ylabel='Observations')
-    ax.yaxis.grid(True)
+    ax.set(xlabel='Category', ylabel='# of Observations')
+    ax.yaxis.grid(True, linewidth=0.33)
     ax.set_xticks([y + 1 for y in range(len(sim_indices))])
     ax.set_xticklabels(loc_names)
-    ax.tick_params(axis='x', rotation=90)
+    ax.tick_params(axis='x', rotation=45)
     plt.tight_layout()
     ani = animation.FuncAnimation(fig, updateviolin, len(dfFull[0][0]))
-        
+    
+    # save the plot
     if save_fig:
        ani.save(filename=plot_folder+'/Overall_Violin.gif', writer="pillow")
 
@@ -317,9 +357,9 @@ def plotNamedStocksByTimestep(code, outdirs, plot_type, FUMEheader):
                     plotLocationDifferences(outdirs, fi, i, sim_indices[i], data_indices[i], loc_names, save_fig=saving, plot_folder=plotfolder, combine_plots_pdf=pdf_pages)
                     fi += 1
     
-            #if plot_type == "loc_hist_gif" or plot_type == "all":
-            #    animateLocationHistogram(outdirs, fi, i, sim_indices[i], data_indices[i], loc_names, y_label, save_fig=saving, plot_folder=plotfolder, combine_plots_pdf=pdf_pages)
-            #    fi += 1
+            if plot_type == "loc_hist_gif" or plot_type == "all":
+               animateLocationHistogram(outdirs, fi, i, sim_indices[i], data_indices[i], loc_names, y_label, save_fig=saving, plot_folder=plotfolder, combine_plots_pdf=pdf_pages)
+               fi += 1
 
         if plot_type == "loc_violin_gif" or plot_type == "all":
             animateLocationViolins(outdirs, fi, i, sim_indices, data_indices, loc_names, y_label, save_fig=saving, plot_folder=plotfolder, combine_plots_pdf=pdf_pages)
@@ -351,3 +391,4 @@ if __name__ == "__main__":
 #       split histograms and violin plots into separate frames as save in a PDF
 
 # histogram: change to log scale (break the Y axis?)
+# animations: try interactive plots to slide or play though days
